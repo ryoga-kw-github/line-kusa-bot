300c51c76e43f7f0bf4ac6ccdce7de12e3b235fd
ryoga_kw <69712737+ryoga-kw-github@users.noreply.github.com>
2020-12-07 22:14:01 +0900

    make index.php-parroting back

--- vendor/linecorp/line-bot-sdk/examples/EchoBot/public/index.php
+++ vendor/linecorp/line-bot-sdk/examples/EchoBot/public/index.php
@@ -1,15 +1,31 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
-$sign = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-$events = $bot->parseEventRequest(file_get_contents('php://input'), $sign);
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 
-foreach ($events as $event) {
-    if (!($event instanceof \LINE\LINEBot\Event\MessageEvent) ||
-        !($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
-        continue;
-    }
-    $bot->replyText($event->getReplyToken(), $event->getText());
-}
+use LINE\LINEBot\EchoBot\Dependency;
+use LINE\LINEBot\EchoBot\Route;
+use LINE\LINEBot\EchoBot\Setting;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$setting = Setting::getSetting();
+$app = new Slim\App($setting);
+
+(new Dependency())->register($app);
+(new Route())->register($app);
+
+$app->run();
