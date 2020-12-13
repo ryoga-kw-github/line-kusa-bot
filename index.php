b0bc87eba0f4cbe8763ac066130ae1fab233a974
ryoga_kw <69712737+ryoga-kw-github@users.noreply.github.com>
2020-12-13 15:04:15 +0900

    index.php , others written code delete&case button

--- index.php
+++ index.php
@@ -12,12 +12,21 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 $sign = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $sign);
-$reply_token = $event->getReplyToken();
 
-/*------------------------------------
+//------------------------------------
+
+$yes_button = new PostbackTemplateActionBuilder('はい', 'button=1');
+$no_button = new PostbackTemplateActionBuilder('キャンセル', 'button=0');
+$actions = [$yes_button, $no_button];
+$button = new ButtonTemplateBuilder('タイトル', 'テキスト', '', $actions);
+$button_message = new TemplateMessageBuilder('タイトル', $button);
+$bot->replyMessage($reply_token, $button_message);
+
+//------------------------------------
 
 foreach ($events as $event) {
-    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {      
+    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
+        $reply_token = $event->getReplyToken();
         $text = $event->getText();
         //$bot->replyText($reply_token, $text);
 
@@ -33,108 +42,6 @@ foreach ($events as $event) {
             case $text === '説明':
                 $bot->replyText($reply_token, '特定の単語を送ると返事してくれます');
                 break;
-
-            case $text === 'ボタン':
-                $yes_button = new PostbackTemplateActionBuilder('はい', 'button=1');
-                $no_button = new PostbackTemplateActionBuilder('キャンセル', 'button=0');
-                $actions = [$yes_button, $no_button];
-                $button = new ButtonTemplateBuilder('タイトル', 'テキスト', '', $actions);
-                $button_message = new TemplateMessageBuilder('タイトル', $button);
-                $bot->replyMessage($reply_token, $button_message);
-                break;
         }
     }
 }
-
-*///------------------------------------
-
-$jsonString = file_get_contents('php://input'); error_log($jsonString); 
-$jsonObj = json_decode($jsonString); $message = $jsonObj->{"events"}[0]->{"message"}; 
-$replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
-
-
- // 送られてきたメッセージの中身からレスポンスのタイプを選択 
-if ($message->{"text"} == '確認') {
-     // 確認ダイアログタイプ 
-    $messageData = [ 
-        'type' => 'template', 
-        'altText' => '確認ダイアログ', 
-        'template' => [ 'type' => 'confirm', 'text' => '元気ですかー？', 
-            'actions' => [
-                [ 'type' => 'message', 'label' => '元気です', 'text' => '元気です' ],
-                [ 'type' => 'message', 'label' => 'まあまあです', 'text' => 'まあまあです' ], 
-            ] 
-        ]
- ]; 
-} elseif ($message->{"text"} == 'ボタン') { 
-    // ボタンタイプ 
-    $messageData = [ 
-        'type' => 'template',
-         'altText' => 'ボタン', 
-        'template' => [
-             'type' => 'buttons',
-             'title' => 'タイトルです',
-             'text' => '選択してね', 
-            'actions' => [
-                 [ 
-                    'type' => 'postback', 
-                    'label' => 'webhookにpost送信', 
-                    'data' => 'value' 
-                ],
-                 [
-                     'type' => 'uri',
-                     'label' => 'googleへ移動', 
-                     'uri' => 'https://google.com' 
-                 ]
-              ]
-          ] 
-     ]; 
-} elseif ($message->{"text"} == 'カルーセル') {
-     // カルーセルタイプ 
-    $messageData = [ 
-        'type' => 'template', 
-        'altText' => 'カルーセル', 
-        'template' => [
-             'type' => 'carousel', 
-            'columns' => [ 
-                [ 
-                    'title' => 'カルーセル1', 
-                    'text' => 'カルーセル1です',
-                     'actions' => [
-                         [
-                            'type' => 'postback',
-                             'label' => 'webhookにpost送信',
-                             'data' => 'value'
-                         ],
-                         [ 
-                            'type' => 'uri', 
-                            'label' => '美容の口コミ広場を見る',
-                             'uri' => 'https://report.clinic/'
-                         ] 
-                    ] 
-                ],
-                 [ 
-                        'title' => 'カルーセル2', 
-                        'text' => 'カルーセル2です', 
-                        'actions' => [ 
-                            [
-                                'type' => 'postback', 
-                                'label' => 'webhookにpost送信', 
-                                'data' => 'value' 
-                            ], 
-                            [ 
-                                'type' => 'uri', 
-                                'label' => '女美会を見る', 
-                                'uri' => 'https://jobikai.com/' 
-                            ] 
-                        ] 
-                    ], 
-                ] 
-            ] 
-    ];
- } else {
-     // それ以外は送られてきたテキストをオウム返し
-     $messageData = [ 'type' => 'text', 'text' => $message->{"text"} ]; 
-} 
-$response = [ 'replyToken' => $replyToken, 'messages' => [$messageData] ]; 
-error_log(json_encode($response)); 
