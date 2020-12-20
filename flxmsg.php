a5ce6723f28cf0cad6ef1b85dbdd84827906c749
ryoga_kw <69712737+ryoga-kw-github@users.noreply.github.com>
2020-12-20 16:28:59 +0900

    flxmsg.php , add confirm case

--- flxmsg.php
+++ flxmsg.php
@@ -8,6 +8,9 @@ use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
 use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
 use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\Event\PostbackEvent;
 
 $httpClient = new CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 $bot = new LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
@@ -18,7 +21,7 @@ $events = $bot->parseEventRequest(file_get_contents('php://input'), $sign);
 $event = $events[0];
 
 
-$reply_token = $event->getReplyToken();
+
 
 $yes_confirm = new PostbackTemplateActionBuilder('はい', 'confirm=1');
 $no_confirm = new PostbackTemplateActionBuilder('いいえ', 'confirm=0');
@@ -30,6 +33,31 @@ $confirm_message = new TemplateMessageBuilder('confirm', $confirm);
 
 $bot->replyMessage($reply_token, $confirm_message);
 
+foreach ($events as $event) {
+  if (!($event instanceof MessageEvent) && !($event instanceof PostbackEvent)) {
+      $reply_token = $event->getReplyToken();
+      $text = $event->getText();
+
+      switch ($text) {
+          case $text === 'どっち':
+              $bot->replyText($reply_token, 'Flxmsg.php');
+              //LINE DevelopersでWebhookにどっちを設定したか判断する
+              break;
+
+          case $text === 'confirm':
+              $yes_confirm = new PostbackTemplateActionBuilder('はい', 'confirm=1');
+              $no_confirm = new PostbackTemplateActionBuilder('いいえ', 'confirm=0');
+
+              $actions = [$yes_confirm, $no_confirm];
+
+              $confirm = new ConfirmTemplateBuilder('メッセージ', $actions);
+              $confirm_message = new TemplateMessageBuilder('confirm', $confirm);
+
+              $bot->replyMessage($reply_token, $confirm_message);
+              break;
+      }
+  }
+}
 
 
 /*
